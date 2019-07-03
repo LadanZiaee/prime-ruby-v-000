38b5fe25b6140d2c9c85d7672f632ca5693c7302
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-03 14:46:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(num)
   prime = *num
-  prime.inc do |isprime|
+  prime.each do |isprime|
     if isprime % !isprime == 0 && isprime % !1 == 0 
       false
     else isprime % 1 == 0 && isprime % isprime == 0 
