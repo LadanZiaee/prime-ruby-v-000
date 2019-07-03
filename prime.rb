7bcc072ef3c15214da5d276a4486b579a5ea7545
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-03 15:03:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   prime = [] 
   prime << num
-  prime.each do |isprime|
+  prime.each.to_i do |isprime|
     if isprime % !isprime == 0 && isprime % !1 == 0 
       false
     elsif isprime % 1 == 0 && isprime % isprime == 0 
