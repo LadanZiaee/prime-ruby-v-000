546b001d221a8898da9c18b1eb611d13209cd8ce
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-03 14:55:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   prime.each do |isprime|
     if isprime % !isprime == 0 && isprime % !1 == 0 
       false
-    else isprime % 1 == 0 && isprime % isprime == 0 
+    elsif isprime % 1 == 0 && isprime % isprime == 0 
       true 
     end
   end
