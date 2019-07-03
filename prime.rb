e9769bf9004b30fb8066182fa93381b499800eff
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-03 15:36:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-def prime?(num)
-  prime = *num
-  prime.each do |isprime|
-    if isprime < 0 || isprime == 0 || isprime == 1
-      false
-    elsif isprime % !isprime == 0 && isprime % !1 == 0 
-      false
-    elsif isprime % 1 == 0 && isprime % isprime == 0 
-      true 
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
     end
+  else
+    false
   end
 end
